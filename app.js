9c761d1b1afdae09c2ffb45773a7a3a068d56516
root <root@ip-10-20-2-242.ap-northeast-2.compute.internal>
2022-03-28 15:25:50 +0000

    app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('junghwan lasst-01d Hello World!'));
+app.get('/', (req, res) => res.send('junghwan lasst-01dd Hello World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
